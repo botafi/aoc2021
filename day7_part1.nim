9b9b37fbd01b8331ed61a2d73026428a9f6a8121
Filip Botai <botaifilip@gmail.com>
2021-12-07 18:14:38 +0100

    day 7

---
+++ day7_part1.nim
@@ -0,0 +1,25 @@
+import std/sequtils
+import strutils
+import std/stats
+import sugar
+import std/math
+
+var positions: seq[float] =
+    lines("data/input-day7.txt")
+    .toSeq[0]
+    .split(",")
+    .map(parseFloat)
+let avg = positions.mean
+let before = positions.filter a => a < avg
+let after = positions.filter a => a > avg
+var avgPoint = (if before.len < after.len: after else: before).mean.round
+let maxPosition = positions.max.toInt
+var bestFuel = positions.foldl(a + abs(b - avgPoint), 0.0)
+for step in 0..maxPosition:
+    let up = positions.foldl(a + abs(b - (avgPoint + step.toFloat)), 0.0)
+    let down = positions.foldl(a + abs(b - (avgPoint - step.toFloat)), 0.0)
+    if up < down and up < bestFuel:
+        bestFuel = up
+    elif down <= up and down < bestFuel:
+        bestFuel = down
+echo bestFuel.toInt
